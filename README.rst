f55b9708cc7e12738b2cf57a3540669a4a01a3c8
Povilas Balciunas <balciunas90@gmail.com>
2017-01-21 13:25:20 +0200

    Use orthographic map

---
+++ README.rst
@@ -0,0 +1,10 @@
+=====
+About
+=====
+
+This is a simple game built with d3js.
+
+References
+==========
+
+* http://bl.ocks.org/mbostock/3757125
--- map.js
+++ map.js
@@ -2,8 +2,9 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 
 var projection = d3.geo
-  .mercator()
-  .scale(150)
+  .orthographic()
+  .scale(300)
+  .clipAngle(90)
   .translate([width / 2, height / 2]);
 
 var path = d3.geo
@@ -15,13 +16,22 @@ var svg = d3.select("#world-map")
   .attr("width", width)
   .attr("height", height);
 
+svg.append("defs").append("path")
+    .datum({type: "Sphere"})
+    .attr("id", "sphere")
+    .attr("d", path);
+svg.append("use")
+    .attr("class", "stroke")
+    .attr("xlink:href", "#sphere");
+
 var g = svg.append("g");
 
-d3.json("countries2.topo.json", function(error, topology) {
-  g.selectAll("path")
+d3.json("countries.json", function(error, topology) {
+  g.selectAll()
     .data(topojson.object(topology, topology.objects.countries)
       .geometries)
       .enter()
       .append("path")
       .attr("d", path)
+      .attr("class", "country")
 });
